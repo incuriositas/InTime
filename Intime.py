49296507745016c55e5e17486be1cff084c4495c
kohyerim <kohyerim78@gmail.com>
2019-10-21 18:30:29 +0900

    weather split

--- Intime.py
+++ Intime.py
@@ -4,9 +4,10 @@ df_181001 = il.mask_with_in1d(il.df_from_cju, "날짜", 20181001)
 
 for i in range(len(il.df_gmp_10)):
     if int(il.df_gmp_10["TM"][i]/100) == 20181001:
-        print(il.df_gmp_10.iloc[i])
-    else:
         pass
+    else:
+        il.df_gmp_10.drop(i, axis=0, inplace=True)
 
 
 col = il.one_hot(il.df_from_cju, "현황")
+print(il.df_gmp_10)
