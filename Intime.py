cca0b4faef2bc9e78da96dcb7251975396788e40
kohyerim <kohyerim78@gmail.com>
2019-10-23 13:12:28 +0900

    module fix

--- Intime.py
+++ Intime.py
@@ -1,23 +1,25 @@
 import IntimeLib as il
 import pandas as pd
+import FlieLib as fl
 
-df_181001 = il.mask_with_in1d(il.df_from_cju, "날짜", 20181001)
+"""
+# 항공기 csv (in FileLib)
+flight_from_cju / flight_from_gmp
+flight_to_cju / flight_to_gmp
+(DataFrame)
 
-for i in range(len(df_181001)):
-    if len(df_181001["계획"][i]) == 4:
-        hour = int(df_181001["계획"][i][0:1])
-    else:
-        hour = int(df_181001["계획"][i][0:2])
+# 날씨 csv (in FileLib)
+weather_cju / weather_gmp
+(List of DataFrame)
+"""
 
-    # print(hour)
+new_df_ = il.month_slice(fl.flight_from_cju, 20181101, 30)
 
-cols = ['현황']
-new_df = il.one_hot(il.flight_from_cju, cols)
-new_df_10 = il.mask_with_in1d(new_df, "날짜", 20181001)
-
-merged_df = pd.merge(new_df_10, il.weather_cju_10, on="TM")
+cols = ['현황','항공사']
+new_df = il.one_hot(new_df_, cols)
+merged_df = pd.merge(new_df, fl.weather_cju[1], on="TM")
 new_merge = merged_df.dropna(axis=1)
-new_merge.drop(["날짜", "Time", "Date", "항공사"], axis=1, inplace=True)
-print(new_merge.head())
-
+new_merge.drop(["날짜", "Time", "Date", "time"],
+               axis=1, inplace=True)
+# print(new_merge.head())
 il.display_heatmap(new_merge)
--- IntimeLib.py
+++ IntimeLib.py
@@ -12,9 +12,10 @@ matplotlib.rc('font', family=font_name)
 
 
 def display_heatmap(df):
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(df.corr(), annot=True ,cmap='cubehelix_r')
+    plt.figure(figsize=(10, 7))
+    sns.heatmap(df.corr(),annot=True ,cmap='cubehelix_r')
     plt.show()
+
 
 def mask_with_in1d(df, column, val):
     mask = np.in1d(df[column].values, [val])
@@ -50,74 +51,10 @@ def fix_weather(df):
     df['Time'] = df.TM.apply(lambda x: str(x)[8:])
 
 
-df_from_cju = pd.read_csv("dataset/SortedCSV/Flight_from_CJU(Sorted).csv")
-df_from_gmp = pd.read_csv("dataset/SortedCSV/Flight_from_GMP(Sorted).csv")
-df_to_cju = pd.read_csv("dataset/SortedCSV/Flight_to_CJU(Sorted).csv")
-df_to_gmp = pd.read_csv("dataset/SortedCSV/Flight_to_GMP(Sorted).csv")
-
-weather_gmp_10 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201810.csv")
-weather_cju_10 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201810.csv")
-weather_gmp_11 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201811.csv")
-weather_cju_11 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201811.csv")
-weather_gmp_12 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201812.csv")
-weather_cju_12 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201812.csv")
-weather_gmp_01 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201901.csv")
-weather_cju_01 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201901.csv")
-weather_gmp_02 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201902.csv")
-weather_cju_02 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201902.csv")
-weather_gmp_03 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201903.csv")
-weather_cju_03 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201903.csv")
-weather_gmp_04 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201904.csv")
-weather_cju_04 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201904.csv")
-weather_gmp_05 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201905.csv")
-weather_cju_05 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201905.csv")
-weather_gmp_06 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201906.csv")
-weather_cju_06 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201906.csv")
-weather_gmp_07 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201907.csv")
-weather_cju_07 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201907.csv")
-weather_gmp_08 = pd.read_csv("dataset/WeatherCSV/김포공항/RKSS_air_stcs201908.csv")
-weather_cju_08 = pd.read_csv("dataset/WeatherCSV/제주공항/RKPC_air_stcs201908.csv")
-
-
-fix_flight(df_from_cju, 0)
-fix_flight(df_from_gmp, 0)
-fix_flight(df_to_cju, 1)
-fix_flight(df_to_gmp, 1)
-
-# fix_time(df_from_cju, "from_cju.csv")
-# fix_time(df_from_gmp, "from_gmp.csv")
-# fix_time(df_to_cju, "to_cju.csv")
-# fix_time(df_to_gmp, "to_gmp.csv")
-
-flight_from_cju = pd.read_csv("dataset/FlightCSV/from_cju.csv")
-flight_from_cju.drop("Unnamed: 0", axis=1, inplace=True)
-flight_from_gmp = pd.read_csv("dataset/FlightCSV/from_gmp.csv")
-flight_from_gmp.drop("Unnamed: 0", axis=1, inplace=True)
-flight_to_cju = pd.read_csv("dataset/FlightCSV/to_cju.csv")
-flight_to_cju.drop("Unnamed: 0", axis=1, inplace=True)
-flight_to_gmp = pd.read_csv("dataset/FlightCSV/to_gmp.csv")
-flight_to_gmp.drop("Unnamed: 0", axis=1, inplace=True)
-
-
-fix_weather(weather_gmp_10)
-fix_weather(weather_cju_10)
-fix_weather(weather_gmp_11)
-fix_weather(weather_cju_11)
-fix_weather(weather_gmp_12)
-fix_weather(weather_cju_12)
-fix_weather(weather_gmp_01)
-fix_weather(weather_cju_01)
-fix_weather(weather_gmp_02)
-fix_weather(weather_cju_02)
-fix_weather(weather_gmp_03)
-fix_weather(weather_cju_03)
-fix_weather(weather_gmp_04)
-fix_weather(weather_cju_04)
-fix_weather(weather_gmp_05)
-fix_weather(weather_cju_05)
-fix_weather(weather_gmp_06)
-fix_weather(weather_cju_06)
-fix_weather(weather_gmp_07)
-fix_weather(weather_cju_07)
-fix_weather(weather_gmp_08)
-fix_weather(weather_cju_08)
+def month_slice(df, day, last):
+    new_df = pd.DataFrame()
+    for i in range(last):
+        temp = mask_with_in1d(df, "날짜", day)
+        new_df = pd.concat([new_df, temp], ignore_index=True)
+        day += 1
+    return new_df
