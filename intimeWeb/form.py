3c4c394f0cd4f14d34d2dd0db6b23dcc201af0cd
kohyerim <kohyerim78@gmail.com>
2019-11-18 18:48:06 +0900

    form fixed

--- intimeWeb/form.py
+++ intimeWeb/form.py
@@ -6,7 +6,7 @@ from bootstrap_datepicker_plus import DatePickerInput, DateTimePickerInput
 class SearchForm(forms.ModelForm):
     class Meta:
         model = Flight
-        fields = ('airline', 'airport', 'arrived', 'date')
+        fields = ('airline', 'airport', 'arrived', 'date', 'delayRate')
         widgets = {
             'date': DateTimePickerInput()
         }
--- intimeWeb/views.py
+++ intimeWeb/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import QueryDict
+
 from .models import Flight
 from django.shortcuts import render, redirect, HttpResponse
 from .form import SearchForm
@@ -6,45 +7,35 @@ from predict import predict_delay, getWeather, get_day
 
 
 def index(request):
-    flights = Flight.objects.all()
-    form = SearchForm()
+    flights = Flight.objects.last()
+    form = SearchForm(request.POST)
     return render(request, 'intimeWeb/index.html', {'flights': flights,
                                                     'form': form})
 
 
-def charts(request):
-    return render(request, 'intimeWeb/charts.html')
-
-
-def search(request, pk):
-    flight = Flight.objects.get(pk=pk)
-    return render(request, 'intimeWeb/search.html', {'flight': flight})
-
-
 def form_func(request):
     flight = Flight.objects.last()
-    year = int(str(flight.date)[0:4])
-    day = int(str(flight.date)[8:10])
-    month = int(str(flight.date)[5:7])
-    hour = int(str(flight.date)[11:13]) + 9
-    weather = getWeather(flight.airport, day, hour)
-    print('year : ' + str(year), 'day : ' + str(day), 'month : ' + str(month) + 'hour : ' + str(hour))
-    print(weather)
-    flight.delayRate = predict_delay(year, month, day, hour,
-                                     get_day(month, day),
-                                     flight.airport,
-                                     flight.airport,
-                                     flight.arrived,
-                                     weather)
-    print(flight.delayRate)
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)  # 중복 DB save를 방지
+            data = form.data.dict()
+            month = int(data['date'][0:2])
+            day = int(data['date'][3:5])
+            year = int(data['date'][6:10])
+            hour = int(data['date'][11:13])
+            weather = getWeather(data['airport'], day, hour)
+            print(weather)
+            data['delayRate'] = predict_delay(year, month, day, hour, get_day(month, day), data['airport'],
+                                              data['airport'],  data['arrived'], weather)
+            print(data['delayRate'])
+            new_dict = QueryDict('', mutable=True)
+            new_dict.update(data)
+            new_form = SearchForm(new_dict)
+            post = new_form.save(commit=False)  # 중복 DB save를 방지
             post.ip = request.META['REMOTE_ADDR']  # ip 필드는 유저로 부터 입력 받지 않고 프로그램으로 채워 넣는다
             post.save()
             return redirect('.')
     else:
         form = SearchForm()
-    return render(request, 'intimeWeb/form.html', {'form': form, 'flight': flight})
+    return render(request, 'intimeWeb/index.html', {'form': form, 'flight': flight})
 
